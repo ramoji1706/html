5583350e0caa2b27c3e246ca22253d2c24e5f279
ramoji706 <ramoji1706@gmail.com>
2024-10-16 16:19:26 +0530

    python files

--- Q33.py
+++ Q33.py
@@ -4,3 +4,21 @@
 #last occurrences: 2
 #replace with: APPLE
 #output:”apple,orange,apple,grape,orange,APPLE,APPLE,orange”
+
+input = "apple,orange,apple,grape,orange,apple,apple,orange"
+source = "apple"
+last_occurrences= 2
+input = input.split(",")
+length = len(input)
+
+newList = []
+count = 0
+revList = input[::-1]
+for i in range(length):
+    if revList[i] == source:
+        revList[i] = source.upper()
+        count +=1
+    if count == last_occurrences:
+        break
+revList = revList[::-1]
+print(",".join(revList))
